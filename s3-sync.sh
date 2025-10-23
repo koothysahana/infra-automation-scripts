25cac99e6e7c6a8e1deec49d1b3dba3a9305491a
EC2 Default User <ec2-user@ip-10-0-1-243.ap-southeast-1.compute.internal>
2025-10-23 18:00:21 +0000

    Added S3 folder sync script with versioning

--- s3-sync.sh
+++ s3-sync.sh
@@ -1,19 +1,9 @@
 #!/bin/bash
-LOCAL_FOLDER=$1
-BUCKET_NAME=$2
 
-if [ -z "$LOCAL_FOLDER" ] || [ -z "$BUCKET_NAME" ]; then
-  echo " Usage: $0 <local-folder> <bucket-name>"
-  echo "Example: ./s3-sync.sh myfolder my-s3-bucket"
-  exit 1
-fi
+LOCAL_FOLDER=~/s3-sync-folder
+S3_BUCKET=s3://sanj-destination-bucket
 
-echo " Upload from '$LOCAL_FOLDER' to bucket '$BUCKET_NAME'"
+aws s3 sync $LOCAL_FOLDER $S3_BUCKET --exact-timestamps --acl private
 
-aws s3api put-bucket-versioning --bucket "$BUCKET_NAME" \
-  --versioning-configuration Status=Enabled
-echo " Versioning is ON to protect  old files."
-
-aws s3 sync "$LOCAL_FOLDER" "s3://$BUCKET_NAME"
-echo " All files uploaded successfully to S3!"
+echo "Sync completed successfully!"
 
